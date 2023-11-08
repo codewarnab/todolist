175e33a7430fec67ddd6ca03c08c64784446436d
blacknut1803 <dp950810@gmail.com>
2023-11-09 01:02:15 +0530

    new file:   images/checked.png 	new file:   images/icon.png 	new file:   images/unchecked.png 	new file:   index.html 	new file:   scricpt.js 	new file:   style.css

---
+++ scricpt.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const list_container = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("you must write something");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML=inputBox.value;
+        list_container.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML='\u00d7';
+        li.appendChild(span);
+    }
+
+    inputBox.value='';
+    saveData();
+}
+
+list_container.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",list_container.innerHTML);
+}
+
+function showTask(){
+    list_container.innerHTML=localStorage.getItem("data");
+}
+
+showTask()
